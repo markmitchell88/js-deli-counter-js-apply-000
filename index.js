080053f9daf316a42edb67137293d1fbf919e8a3
Mark Mitchell <mmitchell88@hotmail.co.uk>
2019-05-09 09:50:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
-function takeANumber(line,name){
-  var name = 'Ada';
-  var line = 1
-  return "Welcome, ${name}. You are number ${line} in line."
+function takeANumber(line, name){
+  katzDeliLine.push("Ada");
+  return `Welcome, ${name}. You are number ${line} in line.`
 }
